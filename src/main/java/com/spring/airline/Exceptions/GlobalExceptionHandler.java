cee26880abd16ae8b534faab3c864048b5ddfc54
Amirabbas <Amir.bhz.w1@gmail.com>
2025-05-11 15:07:42 +0330

    add some exceptions

--- src/main/java/com/spring/airline/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/spring/airline/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.spring.airline.Exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,5 +18,15 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidPhoneNumberException.class)
     public ResponseEntity<String> InvalidPhoneNumberExceptionHandler(InvalidPhoneNumberException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(AlreadyExistException.class)
+    public ResponseEntity<String> handleAlreadyExistException(AlreadyExistException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
     }
 }
---
+++ src/main/java/com/spring/airline/Service/PersonService.java
@@ -0,0 +1,5 @@
+package com.spring.airline.Service;
+
+public class PersonService {
+
+}
